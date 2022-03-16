026d81d21acef39e57410fa24999fabb25cb7ba4
Mustafa Gurkan Canakci <45120405+grknc@users.noreply.github.com>
2022-03-17 02:08:20 +0300

    Update strings.ts

--- src/constants/strings.ts
+++ src/constants/strings.ts
@@ -1,14 +1,14 @@
 export const GAME_TITLE = process.env.REACT_APP_GAME_NAME!
 
 export const WIN_MESSAGES = ['Harika!', 'Başardın', 'Tebrikler!']
-export const GAME_COPIED_MESSAGE = 'Game copied to clipboard'
+export const GAME_COPIED_MESSAGE = 'Panoya kopyaladınız'
 export const NOT_ENOUGH_LETTERS_MESSAGE = 'Harf yeterli değil'
 export const WORD_NOT_FOUND_MESSAGE = 'Oyuncu bulunamadı'
 export const HARD_MODE_ALERT_MESSAGE =
-  'Hard Mode can only be enabled at the start!'
+  'Başlangıç ekranında zor mod seçilebilir!'
 export const HARD_MODE_DESCRIPTION =
-  'Any revealed hints must be used in subsequent guesses'
-export const HIGH_CONTRAST_MODE_DESCRIPTION = 'For improved color vision'
+  'Oluşan ipuçları sonraki tahminlerinizde kullanılmaktadır.'
+export const HIGH_CONTRAST_MODE_DESCRIPTION = 'Renklendirilmiş görüntüye erişebilirsiniz.'
 export const CORRECT_WORD_MESSAGE = (solution: string) =>
   `The word was ${solution}`
 export const WRONG_SPOT_MESSAGE = (guess: string, position: number) =>
@@ -18,10 +18,10 @@ export const NOT_CONTAINED_MESSAGE = (letter: string) =>
 export const ENTER_TEXT = 'Giriş'
 export const DELETE_TEXT = 'Sil'
 export const STATISTICS_TITLE = 'İstatistik'
-export const GUESS_DISTRIBUTION_TEXT = 'Guess Distribution'
+export const GUESS_DISTRIBUTION_TEXT = 'Tahmin Dağılımı'
 export const NEW_WORD_TEXT = 'Yeni Kelimeler'
 export const SHARE_TEXT = 'Paylaş'
-export const TOTAL_TRIES_TEXT = 'Total tries'
+export const TOTAL_TRIES_TEXT = 'Toplam Deneme'
 export const SUCCESS_RATE_TEXT = 'Başarı Oranı'
-export const CURRENT_STREAK_TEXT = 'Current streak'
-export const BEST_STREAK_TEXT = 'Best streak'
+export const CURRENT_STREAK_TEXT = 'Mevcut Galibiyet'
+export const BEST_STREAK_TEXT = 'En İyi Galibiyet'
